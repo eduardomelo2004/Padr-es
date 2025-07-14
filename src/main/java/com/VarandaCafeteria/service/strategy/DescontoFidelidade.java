bbfdf4c59ac1ca829b9f7ec2de7940b68c4491e7
Caiovitorg51 <caio.vitorg51@gmail.com>
2025-07-14 16:40:59 -0300

    calculo do desconto agora garante que o valor retornado tenha apenas uma casa decimal

--- src/main/java/com/VarandaCafeteria/service/strategy/DescontoFidelidade.java
+++ src/main/java/com/VarandaCafeteria/service/strategy/DescontoFidelidade.java
@@ -1,8 +1,14 @@
 package com.VarandaCafeteria.service.strategy;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 // service/strategy/DescontoFidelidade.java
 public class DescontoFidelidade implements DescontoStrategy {
     public double aplicarDesconto(double valor) {
-        return valor * 0.9; // 10% de desconto
+        BigDecimal preco = BigDecimal.valueOf(valor);
+        BigDecimal desconto = preco.multiply(BigDecimal.valueOf(0.10));
+        BigDecimal finalComDesconto = preco.subtract(desconto).setScale(2, RoundingMode.HALF_UP);
+        return finalComDesconto.doubleValue();
     }
 }
--- src/main/java/com/VarandaCafeteria/service/strategy/DescontoPix.java
+++ src/main/java/com/VarandaCafeteria/service/strategy/DescontoPix.java
@@ -1,9 +1,15 @@
 package com.VarandaCafeteria.service.strategy;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 // service/strategy/DescontoPix.java
 public class DescontoPix implements DescontoStrategy {
     public double aplicarDesconto(double valor) {
-        return valor * 0.95; // 5% de desconto
+        BigDecimal preco = BigDecimal.valueOf(valor);
+        BigDecimal desconto = preco.multiply(BigDecimal.valueOf(0.05));
+        BigDecimal finalComDesconto = preco.subtract(desconto).setScale(2, RoundingMode.HALF_UP);
+        return finalComDesconto.doubleValue(); // 5% de desconto
     }
 }
 
